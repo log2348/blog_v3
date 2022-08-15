da5b0df00c2a6903ed21c778b4f83179d1217d30
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-08-15 18:34:06 +0900

    first commit

---
+++ src/main/java/com/example/blog_v3/BlogV3Application.java
@@ -0,0 +1,13 @@
+package com.example.blog_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blog_v3/BlogV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
